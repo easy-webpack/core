b85c7c8b05dd27063e76e534d4b2690253b93dd6
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-06-19 21:54:30 +0200

    fix(index): export all from assign package

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {WebpackConfig} from './webpack'
 import {assign, literalReplace} from '@easy-webpack/assign'
 export {get} from 'lodash'
 export * from './webpack'
+export * from '@easy-webpack/assign'
 export type EasyWebpackConfig = WebpackConfig | ((this: WebpackConfig) => WebpackConfig)
 
 function hasProcessFlag(flag) {
