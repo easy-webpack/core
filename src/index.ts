5998808f61a227b41ae0d19deefe60f98813887f
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-11-12 14:32:03 +0100

    feat(index): implement a merge function with simpler syntax
    BREAKING CHANGE: generateConfig only adds metadata when it does not already exist!
    
    Set generateConfigOptions.alwaysAddBaseMetadata = true to go back to previous behavior.
    
    You can now also turn off generateConfig merging metadata altogether.

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,63 @@ function hasProcessFlag(flag) {
   return process.argv.join('').indexOf(flag) > -1
 }
 
+export const mergeSummary = { dependencies: [], merged: [], skipped: [] }
+export interface ConfigDescription {
+  name?: string
+  dependencies?: Array<string>
+  description?: string
+  enabled?: undefined | boolean | ((config?: WebpackConfig) => boolean)
+  action?: 'append' | 'prepend' | 'replace' | ((previousConfig: WebpackConfig, thisConfig?: WebpackConfig, name?: string) => WebpackConfig)
+}
+
+export const description = '📄'
+export type WebpackConfigWithDescription = WebpackConfig & { '📄'?: ConfigDescription }
+
+export function merge(config: WebpackConfig, ...configs: Array<WebpackConfigWithDescription>) {
+  mergeSummary.dependencies = []
+  mergeSummary.merged = []
+  mergeSummary.skipped = []
+  let i = 0
+  for (let overlayConfig of configs) {
+    let configDescription = overlayConfig[description] || { }
+    let name = configDescription.name || `unnamed config ${String(i++)}`
+    let enabled = configDescription.enabled
+    let action = configDescription.action || 'append'
+    if (configDescription.dependencies && configDescription.dependencies.length) {
+      mergeSummary.dependencies.push(...overlayConfig[description].dependencies)
+    }
+    delete overlayConfig[description]
+    if (enabled === undefined || enabled === true || (typeof enabled === 'function' && enabled(config))) {
+      config = typeof action === 'function' ? action(config, overlayConfig, name) : assign(config, overlayConfig, name, action)
+      mergeSummary.merged.push(name)
+    } else {
+      mergeSummary.skipped.push(name)
+    }
+  }
+  return config
+}
+
+/**
+ * Below are backwards compatibile easy-webpack configs:
+ */
+
+/**
+ * A webpack config object with optional 'metadata'
+ */
 export type WebpackConfigWithMetadata = WebpackConfig & { metadata?: any }
 export type EasyWebpackConfig = WebpackConfigWithMetadata | ((this: WebpackConfigWithMetadata) => WebpackConfigWithMetadata)
+export const generateConfigOptions = { addDefaultMetadata: true, alwaysAddBaseMetadata: false }
 
 export function generateConfig(...configs: Array<EasyWebpackConfig>) {
-  let config = {
-    metadata: {
+  let config = {} as WebpackConfigWithMetadata
+  if (generateConfigOptions.alwaysAddBaseMetadata || (!config.metadata && generateConfigOptions.addDefaultMetadata)) {
+    config.metadata = {
       port: process.env.WEBPACK_PORT || 9000,
       host: process.env.WEBPACK_HOST || 'localhost',
       ENV: process.env.NODE_ENV || process.env.ENV || 'development',
       HMR: hasProcessFlag('hot') || !!process.env.WEBPACK_HMR,
     }
-  } as WebpackConfigWithMetadata
+  }
 
   for (let configMethod of configs) {
     if (typeof configMethod === 'function') {
